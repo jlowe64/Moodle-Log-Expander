c06e5210c13dad6f2de64055fe0bfeaca2680395
Ryan Smith <0ryansmith1994@gmail.com>
2015-08-07 12:43:42 +0100

    Corrects test repository for use in plugin.

--- tests/TestRepository.php
+++ tests/TestRepository.php
@@ -11,22 +11,22 @@ class TestRepository extends MoodleRepository {
      */
     protected function readStore($type, array $query) {
         return (object) [
-            'id' => 1,
+            'id' => '1',
             'username' => 'test_username',
-            'lang' => 'test_lang',
+            'lang' => 'en',
             'fullname' => 'test_fullname',
             'summary' => 'test_summary',
             'name' => 'test_name',
             'intro' => 'test_intro',
             'timestart' => 1433946701,
             'timefinished' => 1433946702,
-            'course' => 1,
-            'sumgrades' => 1,
-            'grade' => 2,
-            'quiz' => 1,
-            'assignment' => 1,
-            'userid' => 1,
-            'forum' => 1,
+            'course' => '1',
+            'sumgrades' => '1',
+            'grade' => '2',
+            'quiz' => '1',
+            'assignment' => '1',
+            'userid' => '1',
+            'forum' => '1',
             'type' => 'object',
         ];
     }
